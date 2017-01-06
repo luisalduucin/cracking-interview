78bdc09a6f981af3697d692a3f718264807b81d0
Luis Daniel <lalducin@nearsoft.com>
2017-01-06 14:44:03 -0600

    Excercise 1.2

--- Chapter1/Chapter1.java
+++ Chapter1/Chapter1.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class Chapter1 {
 
   /*
-    Excercise 1.1 -> hasUniqueChars()
+    Excercise 1.1 -> hasUniqueChars
+    Excercise 1.2 -> arePermutations
   */
 
   public static boolean hasUniqueChars(String word) {
@@ -15,6 +18,16 @@ public class Chapter1 {
       asciiTable[c] = true;
     }
     return true;
+  }
+
+  public static boolean arePermutations(String word1, String word2) {
+    char[] word1Array = word1.toCharArray();
+    char[] word2Array = word2.toCharArray();
+
+    Arrays.sort(word1Array);
+    Arrays.sort(word2Array);
+
+    return Arrays.equals(word1Array, word2Array);
   }
 
 }
